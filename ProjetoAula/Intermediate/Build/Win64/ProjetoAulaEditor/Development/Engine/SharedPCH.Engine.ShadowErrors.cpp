452e5a984d0fdd0358a073e8a4395919e6e27985
Marco <myerco@gmail.com>
2020-10-12 19:24:46 -0400

    fix: Atualizando os diretórios da aplicação

--- ProjetoAula/Intermediate/Build/Win64/ProjetoAulaEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,1 +0,0 @@
-#include "F:/UnrealProjects/ProjetoAula/Intermediate/Build/Win64/ProjetoAulaEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ ProjetoAula/ProjetoAula/Intermediate/Build/Win64/ProjetoAulaEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "F:/UnrealProjects/ProjetoAula/ProjetoAula/ProjetoAula/Intermediate/Build/Win64/ProjetoAulaEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
